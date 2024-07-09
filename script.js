15fdc7e3dfa870d52901a39342e92d65233ba736
Rastko <rastko.strbac@gmail.com>
2024-07-09 17:55:01 +0200

    Create add book functionality
    Create dialog and style it, create form inside and style it. Create
    logic which adds book using form.

--- script.js
+++ script.js
@@ -34,3 +34,31 @@ function displayBooks() {
         libraryWrapper.appendChild(bookDiv);
     }
 }
+
+const newBook = document.getElementById('new-book');
+const addBook = document.getElementById('add-book');
+const dialog = document.getElementById('dialog');
+const dialogClose = document.getElementById('dialog-close');
+
+newBook.addEventListener("click", () => {
+    dialog.style.display = 'flex';
+    dialog.showModal();
+});
+
+dialogClose.addEventListener("click", () => {
+    dialog.close();
+    dialog.style.display = 'none';
+});
+
+const form = document.getElementById('book-form');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let title = document.getElementById('title-input').value;
+    let author = document.getElementById('author-input').value;
+    let pages = document.getElementById('pages-input').value;
+    let read = document.getElementById('read-input').value;
+    const book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+    displayBooks();
+})
