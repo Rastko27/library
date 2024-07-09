dcef386e87ca2abc8a7598243e99ca98778d4024
Rastko <rastko.strbac@gmail.com>
2024-07-09 14:48:44 +0200

    Add basic design

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    var message = "Added " + book.title + " to library.";
+    let message = "Added " + book.title + " to library.";
     return message;
 }
 
+const libraryWrapper = document.getElementById('library-wrapper');
+
 function displayBooks() {
     for(let book of myLibrary) {
-        console.log(book.title + ", written by " + book.author + ", " + book.pages + "pages, " + book.read + ".");
+        let bookTitle = document.createElement('h2')
+        let bookAuthor = document.createElement('p')
+        let bookPages = document.createElement('p')
+        let bookRead = document.createElement('p')
+        let bookDiv = document.createElement('div');
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        bookRead.textContent = book.read;
+        bookDiv.classList.add('book');
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+        bookDiv.appendChild(bookRead);
+        libraryWrapper.appendChild(bookDiv);
     }
 }
